1c961cfc936da009d431e1be280b30d35d870944
wangxianzhuo <1722741150@qq.com>
2017-08-16 23:54:11 +0800

    complete schedule task

--- go-version/main.go
+++ go-version/main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
 	"time"
 )
@@ -11,17 +11,50 @@ import (
 // Suffix means it is the text file
 const Suffix = ".txt"
 
+// SavePath means the draft save path
+var SavePath = ""
+
 func main() {
 	path := flag.String("path", "./draft", "draft path default current")
+	updateTime := flag.String("time", "00:00:00", "update draft time")
+	flag.Parse()
 	_, err := ioutil.ReadDir(*path)
 	if err != nil {
-		fmt.Println("create the dir: " + *path)
+		log.Println("create the dir: " + *path)
 		err = os.Mkdir(*path, 0777)
 	}
-	err = createCurrentDraft(*path)
+	SavePath = *path
+	scheduleTask(*updateTime)
+}
+
+func parseUpdateTime(updateTime string) (time.Time, error) {
+	now := time.Now()
+	t, err := time.ParseInLocation("2006-01-02 15:04:05", now.Format("2006-01-02")+" "+updateTime, time.Local)
+	// t = t.Local()
 	if err != nil {
-		fmt.Println(err)
-		return
+		return time.Time{}, err
+	}
+	if t.Before(now) {
+		t = t.Add(time.Hour * 24)
+	}
+	return t, nil
+}
+
+func scheduleTask(updateTime string) error {
+	for {
+		executeTime, err := parseUpdateTime(updateTime)
+		if err != nil {
+			log.Printf("parse update time error: %v\n", err)
+			continue
+		}
+		log.Printf("%v will create the draft\n", executeTime.Format("2006-01-02 15:04:05"))
+		log.Printf("sleep %v\n", time.Duration(executeTime.Unix()-time.Now().Unix())*time.Second)
+		time.Sleep(time.Duration(executeTime.Unix()-time.Now().Unix()) * time.Second)
+		err = createCurrentDraft(SavePath)
+		if err != nil {
+			log.Printf("create current draft error: %v\n", err)
+			continue
+		}
 	}
 }
 
@@ -35,7 +68,7 @@ func createCurrentDraft(path string) error {
 			return err
 		}
 	}
-	fmt.Println("create current.txt")
+	log.Println("create current.txt")
 	os.Create(currentPath + Suffix)
 	return nil
 }
